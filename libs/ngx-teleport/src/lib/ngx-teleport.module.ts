1ff8222fb215d148310d54d0ec70a8c8b85ef70f
Nivrith <nivrithgomatam@gmail.com>
2021-10-08 19:00:14 +1030

    cleanup

--- libs/ngx-teleport/src/lib/ngx-teleport.module.ts
+++ libs/ngx-teleport/src/lib/ngx-teleport.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeleportComponent } from './components/teleport.component';
-import { PortalModule } from '@angular/cdk/portal';
 import { NgxTeleportOutlet as NgxTeleportOutlet } from './teleport-outlet.directive';
 
 @NgModule({
   declarations: [ TeleportComponent, NgxTeleportOutlet ],
-  imports: [ CommonModule, PortalModule ],
+  imports: [ CommonModule ],
   exports: [ TeleportComponent, NgxTeleportOutlet ],
 })
 export class NgxTeleportModule { }
